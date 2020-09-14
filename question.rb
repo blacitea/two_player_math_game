0d58dfa6509e815d60f7d7a9ef44ca5768bcc2e3
Angel <blacitea@gmail.com>
2020-09-14 16:49:41 -0700

    move player related logic to game

--- question.rb
+++ question.rb
@@ -1,13 +1,14 @@
 class Question
+  attr_reader :number1, :number2, :correct_answer
 
-  def initialize (active_player)
+  def initialize #(active_player)
     @number1 = rand(20)
     @number2 = rand(20)
-    @active_player = active_player
-    @player_answer
+    # @active_player = active_player
+    # @player_answer
     @correct_answer = @number1 + @number2
   end
-
+=begin  Removing player related login to keep question class clean
   def ask
     puts "#{@active_player.name}: What does #{@number1} plus #{@number2} equal?"
     print "> "
@@ -22,5 +23,5 @@ class Question
       @active_player.subtract_lives
     end
   end
-
+=end
 end
