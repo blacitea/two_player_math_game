83ba9368345aad2d69b4e7386952a9866a7be78b
Angel <blacitea@gmail.com>
2020-09-14 13:58:57 -0700

    add to_i for player_answer

--- question.rb
+++ question.rb
@@ -10,11 +10,11 @@ class Question
 
   def ask
     puts "#{@active_player}: What does #{@number1} plus #{@number2} equal?"
-    @player_answer = $stdin.gets.chomp
+    @player_answer = $stdin.gets.chomp.to_i
   end
 
   def is_correct?
-    @user_answer == @correct_answer
+    @player_answer == @correct_answer
   end
 
 end
