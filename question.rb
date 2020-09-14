8ac7f6341ace0ee8a3dc86f7aee67ea085ce81fc
Angel <blacitea@gmail.com>
2020-09-14 13:42:58 -0700

    add logic to call name from active_player

--- question.rb
+++ question.rb
@@ -3,17 +3,18 @@ class Question
   def initialize (active_player)
     @number1 = rand(20)
     @number2 = rand(20)
-    @active_player = active_player
-    @question = "#{@active_player}: What does #{@number1} plus #{@number2} equal?"
+    @active_player = active_player.name
+    @player_answer
     @correct_answer = @number1 + @number2
   end
 
-  def question
-    @question
+  def ask
+    puts "#{@active_player}: What does #{@number1} plus #{@number2} equal?"
+    @player_answer = $stdin.gets.chomp
   end
 
-  def is_correct?(user_answer)
-    user_answer == @correct_answer
+  def is_correct?
+    @user_answer == @correct_answer
   end
 
 end
