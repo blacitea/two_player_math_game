098b678e6d956227bc8a6b1cb0a173e611f332e9
Angel <blacitea@gmail.com>
2020-09-14 14:26:19 -0700

    add winner and end game

--- game.rb
+++ game.rb
@@ -1,26 +1,59 @@
 require_relative 'player.rb'
 require_relative 'question.rb'
 class Game
+  attr_reader :p1, :p2
   def initialize
-    @player_list = [Player.new("Player 1"), Player.new("Player 2")]
-    @active_player = @player_list[0]
+    #@active_player = @player_list[0]
+    @p1 = Player.new("Player 1")
+    @p2 = Player.new("Player 2")
+    @player_list = [p1, p2]
+    @active_player = p1
   end
 
   def new_turn
-    @active_player == @player_list[0]? @active_player = @player_list[1] : @active_player = @player_list[0]
-    "-----NEW TURN-----"
+    if @active_player == p1
+      @active_player = p2
+    else
+      @active_player = p1
+    end
+    puts "-----NEW TURN-----"
   end
 
   def turn
     question = Question.new(@active_player)
     question.ask
-    if question.is_correct?
+    if question.is_correct? == true
       puts "#{@active_player.name}: YES! You are correct."
     else
       puts "#{@active_player.name}: Seriously? NO!"
       @active_player.subtract_lives
     end
-    puts "P1: #{@player_list[0].lives}/3 vs P2: #{@player_list[1].lives}/3"
+    puts "P1: #{p1.lives} vs P2: #{p2.lives}"
+  end
+
+  def next?
+    p1.alive? && p2.alive?
+  end
+
+  def winner
+    winner = @player_list.find {|player| player.alive?}
+    puts ""
+    puts "#{winner.name} wins with a score of #{winner.lives}"
+  end
+
+  def game_over
+    puts "-----GAME OVER-----"
+    puts "Good bye!"
+  end
+
+  def start_game
+    turn
+    while next?
+      new_turn
+      turn
+    end
+    winner
+    game_over
   end
 
 end
